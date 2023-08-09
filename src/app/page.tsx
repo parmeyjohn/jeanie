b88648aba06a24b48af8a5f63a09610adbe7affd
John Cuviello <jcuviell@uci.edu>
2023-08-08 23:31:52 -0700

    added men/women dept div on homescreen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between px-24  mx-auto bg-slate-200">
-      <div className="w-screen h-auto max-w-4xl text-3xl flex justify-center p-2 mb-8">
+      <div className="w-screen h-auto max-w-4xl text-3xl flex justify-center p-2 mb-16">
         <div className="relative rounded-xl w-[60%] h-[40rem] m-2 shadow-lg">
           <div className="absolute w-full h-full transition-all ease-linear duration-100 rounded-xl z-10 bg-indigo-200 opacity-0 hover:opacity-10"></div>
           <Image
@@ -45,7 +45,40 @@ export default function Home() {
         </div>
       </div>
       <div className="max-w-4xl mx-auto">
-        <ProductCarousel title="Featured Items"></ProductCarousel>
+        <div className="mb-16">
+          <ProductCarousel title="Featured Items"></ProductCarousel>
+        </div>
+        <h2 className="text-lg font-semibold">Shop new arrivals:</h2>
+        <div className="flex justify-around items-center p-4 mb-16">
+          <div className="relative border border-indigo-700 rounded-xl h-96 w-96 ">
+            <div className="absolute shadow-2xl hover:shadow-lg cursor-pointer w-full h-full rounded-xl flex justify-center items-center opacity-[0.85] opacity-85 hover:bg-indigo-600 bg-indigo-500 transition-all duration-100 ease-linear z-10">
+              <h2 className="text-indigo-50 uppercase opacity-100 text-3xl font-semibold">
+                Men's
+              </h2>
+            </div>
+            <Image
+              className="rounded-xl opacity-15 object-cover"
+              src="https://images.unsplash.com/photo-1512441547630-6ef146e5af13?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=686&q=80"
+              alt="Man wearing denim jacket"
+              fill={true}
+            ></Image>
+          </div>
+          
+          <div className="relative border border-indigo-700 rounded-xl h-96 w-96 ">
+            <div className="absolute shadow-2xl hover:shadow-lg cursor-pointer w-full h-full rounded-xl flex justify-center items-center opacity-[0.85] opacity-85 hover:bg-indigo-600 bg-indigo-500 transition-all duration-100 ease-linear z-10">
+              <h2 className="text-indigo-50 uppercase opacity-100 text-3xl font-semibold">
+                Women's
+              </h2>
+            </div>
+            <Image
+              className="rounded-xl opacity-15 object-cover object-bottom"
+              src="https://images.unsplash.com/photo-1518825546183-853390a31be3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=737&q=80"
+              alt="Woman wearing denim jacket"
+              fill={true}
+            ></Image>
+          </div>
+
+        </div>
       </div>
     </main>
   );
