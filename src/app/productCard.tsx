eb721469e210baff784dfc490d21c9a85790120d
John Cuviello <jcuviell@uci.edu>
2023-08-15 23:16:30 -0700

    fixed relative pathname for link

--- src/app/productCard.tsx
+++ src/app/productCard.tsx
@@ -4,17 +4,19 @@ import { Product } from "../../types";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
+
 export default function ProductCard({
   price,
   title,
   main_img,
   sec_img,
   id,
+  section
 }: Product) {
   const pathname = usePathname();
 
   return (
-    <Link href={`${pathname}/${id}`}>
+    <Link href={`${process.env.BASE_URL}/${section}s-clothing/${id}`}>
       <div className="m-4 rounded-xl border border-indigo-300 cursor-pointer bg-indigo-100">
         <div className="relative w-60 h-72 transition-all ease-in-out rounded-xl flex flex-col justify-center ">
           <div className="absolute z-10 opacity-0 rounded-t-xl w-full h-full bg-indigo-200 hover:opacity-20 transition-all ease-in-out duration-100"></div>
