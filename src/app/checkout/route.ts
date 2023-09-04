62003817cec955027a485ee619d246aad8e9b842
Nick Cuviello <nickcuv@gmail.com>
2023-09-03 19:08:05 -0700

    fixed stripe api implementation and redirect

--- src/app/checkout/route.ts
+++ src/app/checkout/route.ts
@@ -1,8 +1,7 @@
 import { NextResponse, NextRequest } from 'next/server'
-import Stripe from 'stripe';
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '', {
-    apiVersion: '2022-11-15'
-})
+import Stripe from "stripe"
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {typescript: true, apiVersion: "2023-08-16"});
+
 
 
 
@@ -13,31 +12,46 @@ export async function POST(request:NextRequest) {
         quantity: 3
 
     }
+    var items = []
+    
+    const cart = await request.json()
+    console.log(cart)
+    for (const cartItem of cart) {
+        //console.log(cartItem.currProduct)
+        items.push(
+            {
+                price_data: {
+                    currency: 'usd',
+                    product_data: {
+                        name: cartItem.currProduct.title,
+                        images: [cartItem.currProduct.main_img]
+                    },
+                    unit_amount: Math.round(cartItem.currProduct.price * 100)
+                },
+                quantity: cartItem.quantity,
+            }
+        )
+    }
+    
+    console.log('new items', items)
+    //const cart = JSON.parse()
+    //console.log(cart)
     console.log(Math.round(product.price * 100))
     try {
         const session = await stripe.checkout.sessions.create({
         payment_method_types: ["card"],
             mode: "payment",
-            line_items: [
-            {
-                price_data: {
-                    currency: 'usd',
-                    product_data: {
-                        name: product.name
-                    },
-                    unit_amount: Math.round(product.price * 100)
-                },
-                quantity: product.quantity,
-            },
-            ],
+            line_items: items,
             success_url: `${process.env.BASE_URL}`,
             cancel_url: `${process.env.BASE_URL}`,
         })
-        return NextResponse.json(session.url); 
+        return NextResponse.json({url: session.url}); 
             
     }
         catch (e) {
             console.log(e)
+            return NextResponse.json({ message: e, success: false });
+
         }
        
 
